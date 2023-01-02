ee3a0993d902622ad28fbd0de002dfac25e9939c
Melvin Welton <nivlem65@gmail.com>
2023-01-02 12:35:45 -0600

    take home setup

---
+++ js/signup.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('form');
+const fullNameInput = document.querySelector('#fullname');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const occupationInput = document.querySelector('#occupation');
+const stateInput = document.querySelector('#state');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const fullName = fullNameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const occupation = occupationInput.value;
+  const state = stateInput.value;
+
+  if (!fullName || !email || !password || !occupation || !state) {
+    alert('All fields are required!');
+    return;
+  }
+
+  if (!isValidEmail(email)) {
+    alert('Invalid email address!');
+    return;
+  }
+
+  if (password.length < 8) {
+    alert('Password must be at least 8 characters long!');
+    return;
+  }
+
+  // If all validation checks pass, submit the form
+  form.submit();
+});
+
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
